4b43eda7bd908e4a72b9119ec90edcff212def68
Simon A. F. Lund <safl@safl.dk>
2015-11-16 00:25:29 +0100

    ...

--- util/deps.cmake.sh
+++ util/deps.cmake.sh
@@ -4,7 +4,7 @@ VER=3.4.0
 
 echo "Building cmake"
 pushd /tmp
-wget http://cmake.org/files/v3.4/cmake-$VER.tar.gz
+wget --no-check-certificate http://cmake.org/files/v3.4/cmake-$VER.tar.gz
 tar xzf cmake-$VER.tar.gz
 pushd cmake-$VER && ./configure --prefix=/tmp/tools/cmake && make && make install
 export PATH=/tmp/tools/cmake/bin:$PATH
