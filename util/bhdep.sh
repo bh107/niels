b450947c9ab6df600f8ea005e050b6767d729000
Simon A. F. Lund <safl@safl.dk>
2015-11-15 22:35:07 +0100

    ...

--- util/bhdep.sh
+++ util/bhdep.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Building Bohrium"
-get https://github.com/bh107/bohrium/archive/master.zip
+pushd /tmp
+wget https://github.com/bh107/bohrium/archive/master.zip
 unzip master.zip
-mkdir bohrium
-pushd bohrium
-cmake -DCMAKE_BUILD_TYPE=Release ../bohrium-master
+mkdir bh_build
+pushd bh_build
+cmake -DCMAKE_BUILD_TYPE=Release -DBRIDGE_CIL=OFF -DBRIDGE_NUMCIL=OFF -DFILTER_FUSEPRINTER=OFF -DFILTER_PPRINT=OFF -DVEM_CLUSTER=OFF -DVEM_PROXY=OFF -DEXT_VISUALIZER=OFF -DVE_GPU=OFF -DTEST_CIL=OFF -DBENCHMARK_CIL=OFF ../bohrium-master
 make -j4
 make install
+popd
 popd
